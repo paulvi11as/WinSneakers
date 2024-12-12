cb446eed1a615ef4c8577d6b4b4235fea5e01039
Paul Villas <pvillas@academic.rrc.ca>
2024-12-12 00:16:38 -0600

    Added sizings in the sneakers products

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,4 +10,11 @@ class Product < ApplicationRecord
     ["name", "description", "price", "stock_quantity"]
   end
 
+  def available_sizes
+    sizes = (5.5..12.5).step(0.5).map do |size|
+      stock = [0, *1..5].sample # Random stock; 0 means "Sold Out"
+      { size: size, stock: stock }
+    end
+    sizes
+  end
 end
